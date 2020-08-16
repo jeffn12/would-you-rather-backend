31b98bd9cb9c4cf4ec5effc423711dda816c2bbf
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-16 18:32:22 -0400

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/api/users");
 var questionsRouter = require("./routes/api/questions");
 
 var app = express();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+console.log(db);
 
 app.use(logger("dev"));
 app.use(express.json());
