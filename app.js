28424041051bdee173d8cc0bb062ecbf9581764d
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-16 18:14:33 -0400

    fix: add /api prefix to api routes

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/questions", questionsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/questions", questionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
