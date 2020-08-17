8ff394c6f130d13e5a87f1d3ec94ea50cf5953e2
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-17 16:30:59 -0400

    feat: GET route returns users object

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,30 @@
 var express = require("express");
 var router = express.Router();
 
+const User = require("../../models/user");
+
 /* GET users listing. */
 router.get("/", (req, res, next) => {
-  res.send("respond with a resource");
+  try {
+    User.find({}, (err, users) => {
+      if (err) {
+        res.status(500).json({ message: err.message, err });
+      } else {
+        res.status(200).json({ users: Object.assign({}, users) });
+      }
+    });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "GET /api/users - User.find() had an error...", err });
+  }
 });
+
 /* POST a user. */
 router.post("/", (req, res, next) => {
   res.send("respond with a resource");
 });
+
 /* DELETE a user. */
 router.delete("/", (req, res, next) => {
   res.send("respond with a resource");
