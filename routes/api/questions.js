be0a2b3e6fdfc6b76a904f48227ef0add5062e21
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-17 15:42:00 -0400

    feat: add REST endpoints

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
+/* GET questions listings. */
+router.get("/", (req, res, next) => {
+  res.send("respond with a resource");
+});
+
+/* POST a question. */
+router.post("/", (req, res, next) => {
+  res.send("respond with a resource");
+});
+
+/* DELETE a question listing. */
+router.delete("/", (req, res, next) => {
   res.send("respond with a resource");
 });
 
