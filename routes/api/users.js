bd8ce2755e723bf76de003387956678341b07cec
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-17 15:43:08 -0400

    feat: add REST endpoints

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,15 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
+  res.send("respond with a resource");
+});
+/* POST a user. */
+router.post("/", (req, res, next) => {
+  res.send("respond with a resource");
+});
+/* DELETE a user. */
+router.delete("/", (req, res, next) => {
   res.send("respond with a resource");
 });
 
