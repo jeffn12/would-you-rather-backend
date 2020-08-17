f7f36d531a053f95f1a9f4c6af652aec3c2cfce0
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-17 17:17:26 -0400

    feat: GET route returns object of questions

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -1,9 +1,27 @@
 var express = require("express");
 var router = express.Router();
 
+const Question = require("../../models/question");
+
 /* GET questions listings. */
 router.get("/", (req, res, next) => {
-  res.send("respond with a resource");
+  try {
+    Question.find({}, (err, questions) => {
+      if (err) {
+        res.status(500).json({ message: err.message, err });
+      } else {
+        let questionsObj = {};
+        questions.forEach((question) => {
+          questionsObj[question._id] = question;
+        });
+        res.status(200).json({ questions: questionsObj });
+      }
+    });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "GET /api/users - User.find() had an error...", error });
+  }
 });
 
 /* POST a question. */
