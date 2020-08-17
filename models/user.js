0c8fbf5e21be6bacae879c495fd4b6c8f2424ad1
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-17 15:07:20 -0400

    feat: create userSchema

---
+++ models/user.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  avatarURL: { type: String, required: false },
+  answers: { type: {}, required: true },
+  questions: { type: [String] }
+});
