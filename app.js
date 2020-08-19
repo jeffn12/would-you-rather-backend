4781cf2e4e7afa5c1ed396d4e5fab0e5e481c97a
Jeff Neuberger <jeffreyneuberger@gmail.com>
2020-08-19 17:10:47 -0400

    feat: update cors to allow hosted frontend

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: ["http://localhost:3000", /jn-would-you-rather/] }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
